e1d35d29ec3ed26cf85cca61a7ee73a8171ded23
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-06-17 11:54:50 +0800

    latest

--- user/medical_dashboard.php
+++ user/medical_dashboard.php
@@ -40,6 +40,19 @@ $donations = mysqli_fetch_all($result2, MYSQLI_ASSOC);
       height: 100%;
       width: 100%;
     }
+
+    .card {
+      background-color: blanchedalmond;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px; /* Add margin between rows */
+    }
+
+    .card-body {
+      padding: 10px;
+      flex-grow: 1;
+    }
   </style>
 </head>
 
@@ -65,87 +78,90 @@ $donations = mysqli_fetch_all($result2, MYSQLI_ASSOC);
             </div>
           </div>
         </form>
-        <h2>Medical fund</h2>
-        <?php
-        $count = 0;
-        foreach ($animals as $animal) :
-          if ($count % 2 == 0) {
-            echo '<div class="row mt-4">';
-          }
-        ?>
-          <div class="col-md-12 p-3">
-            <div class="card shadow clickable-card" style="width: 100%; background-color: blanchedalmond; height: auto; overflow: hidden; background-clip: border-box;">
-              <div class="row no-gutters" onclick="window.location.href='animal_profile.php?id=<?php echo $animal['id']; ?>';">
-                <div class="col-md-6">
-                  <div style="
+        <div class="text-center">
+          <h2>Medical fund</h2>
+        </div>
+        <div class="row mt-4">
+          <?php
+          $count = 0;
+          foreach ($animals as $animal) :
+          ?>
+            <div class="col-md-6">
+              <div class="card shadow clickable-card">
+                <div class="row no-gutters" onclick="window.location.href='animal_profile.php?id=<?php echo $animal['id']; ?>';">
+                  <div class="col-md-6">
+                    <div style="
                       background-image: url('<?php echo $animal["image_path"]; ?>'); 
                       height: 100%; 
                       background-size: cover; 
-                      background-position: center;">
+                      background-position: center;
+                      background-repeat: no-repeat;
+                      display: flex;
+                      align-items: center;
+                      justify-content: center;">
+                      <img src="<?php echo $animal["image_path"]; ?>" alt="<?php echo $animal["name"]; ?>" class="fill-image">
+                    </div>
                   </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <h5 class="card-title"><?php echo $animal['name']; ?></h5>
-                    <p class="card-text"><?php echo $animal['description']; ?></p>
-                    <p class="card-text">Age (month/year): <?php echo $animal['age']; ?> years old</p>
-                    <p class="card-text">Gender: <?php echo $animal['gender']; ?></p>
-                    <p class="card-text">Breed: <?php echo $animal['breed']; ?></p>
-                    <p class="card-text">Maturing Size: <?php echo $animal['maturing_size']; ?></p>
-                    <p class="card-text">Vaccinated: <?php echo $animal['vaccinated'] ? 'Yes' : 'No'; ?></p>
-                    <p class="card-text">Medical Fee: <?php echo $animal['medical_adopt_fee']; ?></p>
-                    <!-- Rest of your code here... -->
-                    <?php
-                    $donationPercentage = 0;
-                    $found              = false;
-                    foreach ($donations as $donation) {
-                      if ($donation['animal_id'] == $animal['id']) {
-                        $found              = true;
-                        $donationPercentage = (float) $donation['total_donations'] / (float) $animal['medical_adopt_fee'] * 100;
-                        break;
+                  <div class="col-md-6">
+                    <div class="card-body">
+                      <h5 class="card-title"><?php echo $animal['name']; ?></h5>
+                      <p class="card-text"><?php echo $animal['description']; ?></p>
+                      <p class="card-text">Age (month/year): <?php echo $animal['age']; ?> years old</p>
+                      <p class="card-text">Gender: <?php echo $animal['gender']; ?></p>
+                      <p class="card-text">Breed: <?php echo $animal['breed']; ?></p>
+                      <p class="card-text">Maturing Size: <?php echo $animal['maturing_size']; ?></p>
+                      <p class="card-text">Vaccinated: <?php echo $animal['vaccinated'] ? 'Yes' : 'No'; ?></p>
+                      <p class="card-text">Medical Fee: <?php echo $animal['medical_adopt_fee']; ?></p>
+                      <!-- Rest of your code here... -->
+                      <?php
+                      $donationPercentage = 0;
+                      $found = false;
+                      foreach ($donations as $donation) {
+                        if ($donation['animal_id'] == $animal['id']) {
+                          $found = true;
+                          $donationPercentage = (float) $donation['total_donations'] / (float) $animal['medical_adopt_fee'] * 100;
+                          break;
+                        }
                       }
-                    }
-                    ?>
-                    <form action="medical_fund.php">
-                      <div class="form-group">
-                        <input type="hidden" name='animal_id' value="<?php echo $animal['id'] ?>">
-                        <a href="<?php echo $donationPercentage >= 100 ? '#' : 'medical_fund.php?id=' . $animal['id'] ?>" class='<?php echo $donationPercentage >= 100 ? 'btn btn-success disabled' : 'btn btn-danger' ?>' onclick="<?php echo $donationPercentage < 100 ? 'return confirm(\'Are you sure?\')' : '' ?>" <?php echo $donationPercentage >= 100 ? 'disabled' : '' ?>>
-                          <?php echo $donationPercentage >= 100 ? 'Case Completed' : 'Need Help' ?>
-                        </a>
-                      </div>
-                    </form>
-                    <?php
-                    if ($found) {
-                    ?>
-                      <div class="progress mt-4">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $donationPercentage ?>%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo round($donationPercentage) ?>%</div>
-                      </div>
-                    <?php
-                    }
-                    ?>
+                      ?>
+                      <form action="medical_fund.php">
+                        <div class="form-group">
+                          <input type="hidden" name='animal_id' value="<?php echo $animal['id'] ?>">
+                          <a href="<?php echo $donationPercentage >= 100 ? '#' : 'medical_fund.php?id=' . $animal['id'] ?>" class='<?php echo $donationPercentage >= 100 ? 'btn btn-success disabled' : 'btn btn-danger' ?>' onclick="<?php echo $donationPercentage < 100 ? 'return confirm(\'Are you sure?\')' : '' ?>" <?php echo $donationPercentage >= 100 ? 'disabled' : '' ?>>
+                            <?php echo $donationPercentage >= 100 ? 'Case Completed' : 'Need Help' ?>
+                          </a>
+                        </div>
+                      </form>
+                      <?php
+                      if ($found) {
+                      ?>
+                        <div class="progress mt-4">
+                          <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $donationPercentage ?>%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo round($donationPercentage) ?>%</div>
+                        </div>
+                      <?php
+                      }
+                      ?>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        <?php
-          $count++;
-          if ($count % 2 == 0) {
-            echo "</div>";
-          }
-        endforeach;
-        if ($count % 2 != 0) {
-          echo "</div>";
-        }
-        ?>
+          <?php
+            $count++;
+            if ($count % 2 == 0) {
+              echo '</div><div class="row mt-4">';
+            }
+          endforeach;
+          ?>
+        </div>
       </div>
     </div>
   </div>
 
-  </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
+
